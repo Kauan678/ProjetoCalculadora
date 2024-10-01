ae048a2849cab7d31db3760c50ee29f75a7057c0
gabrielraulino <gabrielviana@alu.ufc.br>
2024-10-01 18:39:44 -0300

    feat: add client

---
+++ ProjCalculadora-Client/client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css'
+import Calculadora from './components/Calculadora'
+
+function App() {
+  return (
+    <> 
+      <Calculadora/>
+    </>
+  )
+}
+export default App
---
+++ ProjCalculadora-Client/client/src/components/Calculadora.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Calculadora = () => {
+    const numero = 42
+
+    return (
+        <div>
+            <h1>O número eh: {numero}</h1>
+        </div>
+    )
+}
+
+export default Calculadora
