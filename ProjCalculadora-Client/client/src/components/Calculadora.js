509b34b1db76e9593da681e90f70769171815b90
gabrielraulino <gabrielviana@alu.ufc.br>
2024-10-04 20:58:41 -0300

    feat: teclado numerico com MUI

--- ProjCalculadora-Client/client/src/components/Calculadora.js
+++ ProjCalculadora-Client/client/src/components/Calculadora.js
@@ -1,16 +1,41 @@
-import React from 'react'
+import React from "react";
+import { Container, Typography } from "@mui/material";
+import { Button, Grid } from "@mui/material";
 
 const Calculadora = () => {
-    const numero = 42
+  return (
+    <Container>
+      <Typography variant="h4" gutterBottom>
+        Calculadora
+      </Typography>
+      <Grid container spacing={2}>
+        {["7", "8", "9", "4", "5", "6", "1", "2", "3", "0", ".", "="].map(
+          (value) => (
+            <Grid item xs={4} key={value}>
+              <Button variant="contained" fullWidth>
+                {value}
+              </Button>
+            </Grid>
+          )
+        )}
+        <br />
+        <Grid
+          container
+          spacing={2}
+          justifyContent="center"
+          style={{ marginBottom: "20px", marginTop: "0px" }} 
+        >
+          {["+", "-", "*", "/"].map((operator) => (
+            <Grid item xs={2.93} key={operator}>
+              <Button variant="contained" fullWidth>
+                {operator}
+              </Button>
+            </Grid>
+          ))}
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
 
-    return (
-        <div>
-            <h1>O número eh: {numero}</h1>
-            <h1>O numero + 1 é: {numero+1} </h1>
-            <h1>O numero + 2 é: {numero+2}</h1>
-        </div>
-        
-    )
-}
-
-export default Calculadora
+export default Calculadora;
