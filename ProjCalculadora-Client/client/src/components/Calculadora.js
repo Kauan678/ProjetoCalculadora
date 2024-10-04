3c5668fc17172a4631086b4a074a9c4cafc8c514
Kauan Perdigão <kauanopl@alu.ufc.br>
2024-10-04 18:52:58 -0300

    testes

--- ProjCalculadora-Client/client/src/components/Calculadora.js
+++ ProjCalculadora-Client/client/src/components/Calculadora.js
@@ -8,8 +8,9 @@ const Calculadora = () => {
             <h1>O número eh: {numero}</h1>
             <h1>O numero + 1 é: {numero+1} </h1>
             <h1>O numero + 2 é: {numero+2}</h1>
+            <h1>O numero + 3 é: {numero+3}</h1>
+            <h1>O numero + 1.000 é: {numero+1000}</h1>
         </div>
-        
     )
 }
 
